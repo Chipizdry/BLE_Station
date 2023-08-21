7ae14579ce45786a51847bca26e6be0b790d1313
Chipizdry <79665980+Chipizdry@users.noreply.github.com>
2023-08-21 10:48:59 +0300

    21.08.2023_Start_progect

---
+++ BLE_Custom/STM32CubeIDE/Debug/Middlewares/STM32_WPAN/osal.d
@@ -0,0 +1,4 @@
+Middlewares/STM32_WPAN/osal.o: \
+ C:/Users/chipi/Downloads/en.stm32cubewb-v1-17-0/STM32Cube_FW_WB_V1.17.0/Middlewares/ST/STM32_WPAN/ble/core/template/osal.c \
+ C:/Users/chipi/Downloads/en.stm32cubewb-v1-17-0/STM32Cube_FW_WB_V1.17.0/Middlewares/ST/STM32_WPAN/ble/core/template/osal.h
+C:/Users/chipi/Downloads/en.stm32cubewb-v1-17-0/STM32Cube_FW_WB_V1.17.0/Middlewares/ST/STM32_WPAN/ble/core/template/osal.h:
---
+++ BLE_Custom/STM32CubeIDE/Debug/Utilities/stm32_seq.d
@@ -0,0 +1,10 @@
+Utilities/stm32_seq.o: \
+ C:/Users/chipi/Downloads/en.stm32cubewb-v1-17-0/STM32Cube_FW_WB_V1.17.0/Utilities/sequencer/stm32_seq.c \
+ C:/Users/chipi/Downloads/en.stm32cubewb-v1-17-0/STM32Cube_FW_WB_V1.17.0/Utilities/sequencer/stm32_seq.h \
+ ../../Core/Inc/utilities_conf.h \
+ ../../../../../../../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../../../../../../../Drivers/CMSIS/Include/cmsis_gcc.h
+C:/Users/chipi/Downloads/en.stm32cubewb-v1-17-0/STM32Cube_FW_WB_V1.17.0/Utilities/sequencer/stm32_seq.h:
+../../Core/Inc/utilities_conf.h:
+../../../../../../../Drivers/CMSIS/Include/cmsis_compiler.h:
+../../../../../../../Drivers/CMSIS/Include/cmsis_gcc.h:
