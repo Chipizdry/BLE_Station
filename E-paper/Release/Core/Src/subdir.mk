c1f06c0cf208b3a9e784ceca0418094a3851135a
Chipizdry <79665980+Chipizdry@users.noreply.github.com>
2023-08-22 13:03:13 +0300

    22.08.2023

---
+++ E-paper/Release/Core/Src/subdir.mk
@@ -0,0 +1,63 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/DEV_Config.c \
+../Core/Src/EPD_2in9.c \
+../Core/Src/EPD_2in9_V2.c \
+../Core/Src/EPD_2in9_V2_test.c \
+../Core/Src/EPD_2in9_test.c \
+../Core/Src/GUI_Paint.c \
+../Core/Src/ImageData.c \
+../Core/Src/main.c \
+../Core/Src/stm32f4xx_hal_msp.c \
+../Core/Src/stm32f4xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32f4xx.c 
+
+OBJS += \
+./Core/Src/DEV_Config.o \
+./Core/Src/EPD_2in9.o \
+./Core/Src/EPD_2in9_V2.o \
+./Core/Src/EPD_2in9_V2_test.o \
+./Core/Src/EPD_2in9_test.o \
+./Core/Src/GUI_Paint.o \
+./Core/Src/ImageData.o \
+./Core/Src/main.o \
+./Core/Src/stm32f4xx_hal_msp.o \
+./Core/Src/stm32f4xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32f4xx.o 
+
+C_DEPS += \
+./Core/Src/DEV_Config.d \
+./Core/Src/EPD_2in9.d \
+./Core/Src/EPD_2in9_V2.d \
+./Core/Src/EPD_2in9_V2_test.d \
+./Core/Src/EPD_2in9_test.d \
+./Core/Src/GUI_Paint.d \
+./Core/Src/ImageData.d \
+./Core/Src/main.d \
+./Core/Src/stm32f4xx_hal_msp.d \
+./Core/Src/stm32f4xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32f4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -DUSE_HAL_DRIVER -DSTM32F401xE -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -Os -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/DEV_Config.cyclo ./Core/Src/DEV_Config.d ./Core/Src/DEV_Config.o ./Core/Src/DEV_Config.su ./Core/Src/EPD_2in9.cyclo ./Core/Src/EPD_2in9.d ./Core/Src/EPD_2in9.o ./Core/Src/EPD_2in9.su ./Core/Src/EPD_2in9_V2.cyclo ./Core/Src/EPD_2in9_V2.d ./Core/Src/EPD_2in9_V2.o ./Core/Src/EPD_2in9_V2.su ./Core/Src/EPD_2in9_V2_test.cyclo ./Core/Src/EPD_2in9_V2_test.d ./Core/Src/EPD_2in9_V2_test.o ./Core/Src/EPD_2in9_V2_test.su ./Core/Src/EPD_2in9_test.cyclo ./Core/Src/EPD_2in9_test.d ./Core/Src/EPD_2in9_test.o ./Core/Src/EPD_2in9_test.su ./Core/Src/GUI_Paint.cyclo ./Core/Src/GUI_Paint.d ./Core/Src/GUI_Paint.o ./Core/Src/GUI_Paint.su ./Core/Src/ImageData.cyclo ./Core/Src/ImageData.d ./Core/Src/ImageData.o ./Core/Src/ImageData.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/stm32f4xx_hal_msp.cyclo ./Core/Src/stm32f4xx_hal_msp.d ./Core/Src/stm32f4xx_hal_msp.o ./Core/Src/stm32f4xx_hal_msp.su ./Core/Src/stm32f4xx_it.cyclo ./Core/Src/stm32f4xx_it.d ./Core/Src/stm32f4xx_it.o ./Core/Src/stm32f4xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f4xx.cyclo ./Core/Src/system_stm32f4xx.d ./Core/Src/system_stm32f4xx.o ./Core/Src/system_stm32f4xx.su
+
+.PHONY: clean-Core-2f-Src
+
